707cc6f4e94be61a87380d60adba2725114a3b76
Benny254 <asshimbenny01@mail.com>
2023-11-18 08:45:22 -0800

    update

--- div.c
+++ div.c
@@ -11,11 +11,11 @@ void f_div(stack_t **head, unsigned int counter)
 	int len = 0, aux;
 
 	h = *head;
-	for (len = 0; h; len++)
+	while (h)
 	{
-	h = h->next;
+		h = h->next;
+		len++;
 	}
-
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
@@ -24,19 +24,17 @@ void f_div(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-
-		h = *head;
-		if (h->n == 0)
-		{
+	h = *head;
+	if (h->n == 0)
+	{
 		fprintf(stderr, "L%d: division by zero\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
-		}
-
-		aux = h->next->n / h->n;
-		h->next->n = aux;
-		*head = h->next;
-		free(h);
+	}
+	aux = h->next->n / h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
 }
